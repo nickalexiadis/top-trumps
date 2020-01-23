2ef4f616f1a020b68d3812b5d4d0b991c3be9c5f
Miruna Lazăr <56715525+mirunalazar3@users.noreply.github.com>
2020-01-23 18:41:01 +0000

    Update CardDeck.java
    I still don't know how to check for a draw in this one and how to then add them to commonPile

--- CardDeck.java
+++ CardDeck.java
@@ -1,40 +1,43 @@
 package commandline;
+import java.util.ArrayList;
 
 
-import java.util.Random;
-
+/* This class will shuffle the card deck. This class will also check for a draw by comparing the 5 cards attributes, and will further place the
+ *  cards that were satisfying the draw condition within the common pile. 
+ */
 public class CardDeck {
-	        
-			//shuffles the card deck, <<reference needed>>
-			public void shuffleCards(int cards[], int n) {
-				
-				Random random = new Random();
-				
-				for (int i = 0; i < n; i++){
-					
-					int randomShuffle = i + random.nextInt(40 - i);
-					int temp = cards[randomShuffle]; //used to iterate the value of the Card array
-					cards[randomShuffle] = cards[i];
-					cards[i] = temp;
-				}	
+	
+	//deck size is always 40
+    private int DECK_SIZE=40;
+    //initial deck, empty
+ 	private ArrayList<Integer> deck = new ArrayList<Integer>();
+			
+	       //shuffles the card deck, reference: https://stackoverflow.com/questions/39557701/shuffle-a-deck-of-cards-in-java
+			public void shuffleCards() {
+			
+		        //fill the deck
+				for (int i = 0; i < DECK_SIZE; ++i) {
+		            deck.add(i);
+		        }
+				//the shuffled deck array list where the shuffled deck will be stored 
+		        ArrayList<Integer> shuffledDeck = new ArrayList<Integer>();
+		        //shuffles the initial deck
+		        while (deck.size() > 0) {
+		            int index = (int) (Math.random() * deck.size());
+		            shuffledDeck.add(deck.remove(index));
+		        }
 			}
 			
-			//if there is a draw, a card is taken and placed in the common pile
-			public void commonPile() {
+			//method to check for draw
+			public static boolean checkForDraw(Cards[] card) {
+				
 				
 			}
+	
 			
-
-			//deals the card deck (in the GamePlay) <<reference needed>>
-			public Cards[] dealCards() {
+			//the cards that were checked in the previous method are now placed in the common pile
+			public void addCommonPile() {
 				
-				Cards[] drawnCards = new Cards[numCards];
-			    
-				for(int index = 0; index < numCards; index ++) {
-			            drawnCards[index] = cards[topCard];
-			            topCard++;
-			        }
-			        return drawnCards;
 			}
 			 
 }
