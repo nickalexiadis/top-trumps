8536d59325c9f625596b3df76218ba888aa43d09
Kwabena-Antwi <abranteeba@gmail.com>
2020-01-25 14:42:36 +0000

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,33 +1,49 @@
-package commandline;
 
 
 public class Player {
 		
-			private String playerID;
+			private int playerID;
 			private int winCounter; 
 			private int numberOfPlayers;
-			private Cards[] hand; 
+			private CardDeck hand; 
+			private Card topCard; 
 			
 		//constructor for the player and how many cards the player has
-		public Player(String playerID, Cards[] hand){
+		public Player(int playerID, CardDeck hand){
 			this.playerID = playerID;
 			this.hand = hand;
 				
 		}	
 			
-		//getter for name
-		public String getPlayerID(){
+		//getter for playerID which is index of the players in the game
+		public int getPlayerID(){
 			return playerID;
 		}
-		//string value of name
-		public String toString(){
-			return getPlayerID();
-		}
+//		
+//		//string value of name:
+//		public String toString(){
+//			return getPlayerID();
+//		}
 		
 		
 		//places all players' cards at the bottom of the deck IF one of the players is the winner of the round
 		public void placeCard() {
 			
 		}
+
+		public CardDeck getHand() {
+			return hand;
+		}
+
+		public void setHand(CardDeck hand) {
+			this.hand = hand;
+		}
+		public Card getTopCard() {
+			return hand.getTopCard();
+			
+		}
+
+		
+	
 		
 }
