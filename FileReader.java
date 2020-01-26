9ce4545b9a297790115d76b132523cf51a3393c3
Miruna Lazăr <56715525+mirunalazar3@users.noreply.github.com>
2020-01-26 13:55:57 +0000

    Update FileReader.java

--- FileReader.java
+++ FileReader.java
@@ -1,63 +1,45 @@
 package commandline;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.IOException;
 
-import online.GUI.Card;
 public class FileReader {
-	public static void main(String[] args) throws FileNotFoundException {
-	    Scanner input = new Scanner(new File("StarCitizenDeck.txt"));
-	    input.useDelimiter("-|\n");
+	
+		public static void main(String[] args) {
+			
+			
+	        File file = new File("StarCitizenDeck.txt");
 
-	    Card[] cards = new Card[0];
-	    while(input.hasNext()) {
-	        String description = input.next();
-	        int size = input.nextInt();
-	        int speed = input.nextInt();
-	        int cargo = input.nextInt();
-	        int range = input.nextInt();
-	        int firepower = input.nextInt();
+	        BufferedReader bufferedReader = null;
 
-	        Card newCard = new Card(description, size, speed, cargo, range, firepower);
-	        cards = addCard(cards, newCard);
+	        try {
+	            java.io.FileReader fileReader = new java.io.FileReader(file);
+	            bufferedReader = new BufferedReader(fileReader);
+
+	            String line;
+	            
+	            while( (line = bufferedReader.readLine()) != null ) {
+	                System.out.println(line);
+	            }
+
+	        } catch (FileNotFoundException exception) {
+	            System.out.println("File not found: " + file.toString());
+	        } 
+	        catch (IOException exception) {
+	            System.out.println("Unable to read file: " + file.toString());
+	        }
+	        finally {
+	         
+	        	try {
+	                bufferedReader.close();
+	            }
+	        	catch (IOException e) {
+	                System.out.println("Unable to close file: " + file.toString());
+	            }
+	            catch(NullPointerException exception) {
+	            }
+	        }
 	    }
 
-	    for (Card card : cards) {
-	        System.out.println(card);
-	    }
 	}
-
-	private static Card[] addCard(Card[] cards, Card cardToAdd) {
-	    Card[] newCards = new Card[cards.length + 1];
-	    System.arraycopy(cards, 0, newCards, 0, cards.length);
-	    newCards[newCards.length - 1] = cardToAdd;
-
-	    return newCards;
-	}
-	public static class Card{
-		
-		//private attributes
-		private String description;
-		private int size;
-		private int speed;
-		private int cargo;
-		private int range;
-		private int firepower;
-		
-		//cards constructor	
-		public Card(String description, int size, int speed, int cargo, int range, int firepower) {
-					        
-					        this.description = description;
-					        this.size = size;
-					        this.speed = speed;
-					        this.cargo = cargo;
-					        this.range = range;
-					        this.firepower = firepower;
-					    }
-		 public String toString() {
-		        return String.format("Description: \r\nSize: \r\nSpeed: \r\nCargo: \r\nRange: \r\nFirepower: \r\n", 
-		                description, size, speed, cargo, range, firepower);
-		    }
-	}
-
-}
