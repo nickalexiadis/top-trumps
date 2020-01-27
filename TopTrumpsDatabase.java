28a49b03f564ef535b3d6e4b353287102ea64a7c
Miruna Lazăr <56715525+mirunalazar3@users.noreply.github.com>
2020-01-27 14:29:48 +0000

    Update TopTrumpsDatabase.java
    Finally established the connection, still have to look into the update method and how to close it, but I should have one of the two by tomorrow morning and the other one on Tuesday evening so we can test it.

--- TopTrumpsDatabase.java
+++ TopTrumpsDatabase.java
@@ -6,6 +6,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import com.mysql.jdbc.PreparedStatement;
+import com.sun.xml.internal.fastinfoset.sax.Properties;
+
 public class TopTrumpsDatabase {
 		
 		//database columns, declared as variables 
@@ -17,49 +20,66 @@ public class TopTrumpsDatabase {
 		
 		//defining the attributes for the connection: URL, user and password
 		//create a variable for the connection along with the user ID and password 
-		private String connectionURL = "jdbc:postgresql://localhost/dvdrental";
-		private final String user = "postgres";
-		private final String password = "<add password>";
-
-		    
+		    private String connectionURL = "Add connection URL";
+		    private String username = "Add username";
+		    private String password ="Add password";
 		    
 		//setup the database connection, reference: https://www.postgresqltutorial.com/postgresql-jdbc/connecting-to-postgresql-database/
-		public Connection con() throws SQLException {
-		       
-			Connection connect = null;
-		    
-			try {
-		            connect = (Connection) DriverManager.getConnection(connectionURL, user, password);
+		 private Connection connect() {
+		        Connection connection = null;
+		        try {
+		            connection = DriverManager.getConnection(connectionURL, username, password);
 		            System.out.println("Connected to the PostgreSQL server successfully.");
 		        } catch (SQLException e) {
-		           
-		        	System.out.println(e.getMessage());
-		        	
-		        } finally {
-		        	//after everything is performed, close the connection
-		        	connect.close();
+		            System.out.println(e.getMessage());
 		        }
+		 
+		        return connection;
+		    }
+
+		
+		//closing the database connection
+		public void closeConenction() {
 			
-				return connect;
-		    
-			}
+		}
+		
+		//update the database
+		 public int updateGameStats(int id, String lastName) {
+		        String SQL = "UPDATE actor "
+		                + "SET last_name = ? "
+		                + "WHERE actor_id = ?";
+		 
+		        int affectedrows = 0;
+		 
+		        try (Connection connection = connect();
+		                java.sql.PreparedStatement pstmt = connection.prepareStatement(SQL)) {
+		 
+		            pstmt.setString(1, lastName);
+		            pstmt.setInt(2, id);
+		 
+		            affectedrows = pstmt.executeUpdate();
+		 
+		        } catch (SQLException ex) {
+		            System.out.println(ex.getMessage());
+		        }
+		        return affectedrows;
+		    }
 		
 		//reference for the following getters: https://docs.oracle.com/javase/tutorial/jdbc/basics/processingsqlstatements.html
-		
 		//query to return the number of games played overall  
 		public int getNumberOfGames() throws SQLException {
 			Statement stmt = null;
 			String queryOne = "select SUM(NUMBER_OF_GAMES)" +
 							  "from table"; 
 			try {
-				stmt = con.createStatement();
+				stmt = connect().createStatement();
 				ResultSet rs = stmt.executeQuery(query);
 				while(rs.next()) {
 					int numberOfGames = rs.getInt("NUMBER_OF_GAMES");
 					System.out.println(numberOfGames+ "\t");
 				}
 			} catch (SQLException e) {
-				
+				System.out.println("Failed to show number of games");	
 			}finally {
 				if (stmt != null) { stmt.close();
 			        }
@@ -72,14 +92,14 @@ public class TopTrumpsDatabase {
 			String QueryTwo = "select SUM(COMPUTER_WINS)" + 
 							  "from table";
 			try {
-				stmt = con.createStatement();
+				stmt = connect().createStatement();
 				ResultSet rs = stmt.executeQuery(query);
 				while(rs.next()) {
 					int computerWins = rs.getInt("COMPUTER_WINS");
 					System.out.println(computerWins + "\t");
 				}
 			} catch (SQLException e) {
-				
+				System.out.println("Failed to show number of computer wins");
 			}finally {
 				if (stmt != null) { stmt.close();
 			        }
@@ -92,35 +112,35 @@ public class TopTrumpsDatabase {
 			String QueryThree = "select SUM(HUMAN_WINS)" +
 								"from table";
 			try {
-				stmt = con.createStatement();
+				stmt = connect().createStatement();
 				ResultSet rs = stmt.executeQuery(query);
 				while(rs.next()) {
 					int humanWins = rs.getInt("HUMAN_WINS");
 					System.out.println(humanWins + "\t");
 				}
 			} catch (SQLException e) {
-				
+				System.out.println("Failed to show number of human wins");
 			}finally {
 				if (stmt != null) { stmt.close();
 			        }
 			  }
 		}
 		
-		//query to return the average number of draws 
+		//James, query to return the average number of draws 
 		public double getNumberOfDraws() throws SQLException {
-		Statement stmt = null;
+			Statement stmt = null;
 			String QueryFour = "select AVG(DRAWS)" + 
 							   "from TABLE";
-				
+
 			try {
-				stmt = con.createStatement();
+				stmt = connect().createStatement();
 				ResultSet rs = stmt.executeQuery(query);
 				while(rs.next()) {
 					int numberOfDraws = rs.getInt("DRAWS");
 					System.out.println(numberOfDraws + "\t");
 				}
 			} catch (SQLException e) {
-				
+				System.out.println("Failed to show number of average draws");
 			}finally {
 				if (stmt != null) { stmt.close();
 				}
