50ace0462dba633ccca2d201f130db0ff4a0ac54
Miruna Lazăr <56715525+mirunalazar3@users.noreply.github.com>
2020-01-27 16:01:20 +0000

    Update TopTrumpsDatabase.java
    This is the updated version, it is missing a method of establishing the connection between the game and database.

--- TopTrumpsDatabase.java
+++ TopTrumpsDatabase.java
@@ -6,9 +6,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import com.mysql.jdbc.PreparedStatement;
-import com.sun.xml.internal.fastinfoset.sax.Properties;
-
+//This class will establish the connection between the Database and the game, it will update the stats in the database.
+ 
 public class TopTrumpsDatabase {
 		
 		//database columns, declared as variables 
@@ -25,7 +24,7 @@ public class TopTrumpsDatabase {
 		    private String password ="Add password";
 		    
 		//setup the database connection, reference: https://www.postgresqltutorial.com/postgresql-jdbc/connecting-to-postgresql-database/
-		 private Connection connect() {
+		 public Connection connect() {
 		        Connection connection = null;
 		        try {
 		            connection = DriverManager.getConnection(connectionURL, username, password);
@@ -38,14 +37,21 @@ public class TopTrumpsDatabase {
 		    }
 
 		
-		//closing the database connection
+		//closing the database connection, Question: should the system let the user know with a statement whether the connection closed or 
+		//failed to close?
 		public void closeConenction() {
-			
+			try {
+				connect().close();	
+				//System.out.println("Connection closed.");
+			} catch (SQLException e) {
+				e.printStackTrace();
+				//System.out.println("Connection failed closing.");
+			}
 		}
 		
-		//update the database, not final!
+		//update the database, Question: How do we actually connect the game to the database? So as to update stats with each game played?
 		 public int updateGameStats(int numberOfGames, int computerWins, int humanWins, double numberOfDraws, int numberOfRoundsPlayedInGame) {
-		        String SQL = "UPDATE NUMBER_OF_GAMES, COMPUTER_WINS, HUMAN_WINS, DRAWS, ROUNDS_PLAYED";
+		        String SQL = "UPDATE NUMBER_OF_GAMES, COMPUTER_WINS, HUMAN_WINS, DRAWS, NUMBER_OF_ROUNDS";
 		        int affectedrows = 0;
 		        try (Connection connection = connect();
 		                java.sql.PreparedStatement pstmt = connection.prepareStatement(SQL)) {
@@ -69,7 +75,7 @@ public class TopTrumpsDatabase {
 		public int getNumberOfGames() throws SQLException {
 			Statement stmt = null;
 			String queryOne = "select SUM(NUMBER_OF_GAMES)" +
-							  "from GAME_STATS"; 
+							  "from GameStats"; 
 			try {
 				stmt = connect().createStatement();
 				ResultSet rs = stmt.executeQuery(queryOne);
@@ -90,7 +96,7 @@ public class TopTrumpsDatabase {
 		public int getComputerWins() throws SQLException {
 			Statement stmt = null;
 			String queryTwo = "select SUM(COMPUTER_WINS)" + 
-							  "from GAME_STATS";
+							  "from GameStats";
 			try {
 				stmt = connect().createStatement();
 				ResultSet rs = stmt.executeQuery(queryTwo);
@@ -99,7 +105,7 @@ public class TopTrumpsDatabase {
 					System.out.println(computerWins + "\t");
 				}
 			} catch (SQLException e) {
-				System.out.println("Failed to show number of computer wins");
+				System.out.println("Failed to show number of computer wins.");
 			}finally {
 				if (stmt != null) { stmt.close();
 			        }
@@ -111,7 +117,7 @@ public class TopTrumpsDatabase {
 		public int getHumanWins() throws SQLException {
 			Statement stmt = null;
 			String queryThree = "select SUM(HUMAN_WINS)" +
-								"from GAME_STATS";
+								"from GameStats";
 			try {
 				stmt = connect().createStatement();
 				ResultSet rs = stmt.executeQuery(queryThree);
@@ -120,7 +126,7 @@ public class TopTrumpsDatabase {
 					System.out.println(humanWins + "\t");
 				}
 			} catch (SQLException e) {
-				System.out.println("Failed to show number of human wins");
+				System.out.println("Failed to show number of human wins.");
 			}finally {
 				if (stmt != null) { stmt.close();
 			        }
@@ -128,21 +134,21 @@ public class TopTrumpsDatabase {
 			return humanWins;
 		}
 		
-		//James, query to return the average number of draws 
+		//query to return the average number of draws, done by James
 		public double getNumberOfDraws() throws SQLException {
 			Statement stmt = null;
 			String queryFour = "select AVG(DRAWS)" + 
-							   "from GAME_STATS";
+							   "from GameStats";
 
 			try {
 				stmt = connect().createStatement();
 				ResultSet rs = stmt.executeQuery(queryFour);
 				while(rs.next()) {
-					int numberOfDraws = rs.getInt("DRAWS");
+					double numberOfDraws = rs.getDouble("DRAWS");
 					System.out.println(numberOfDraws + "\t");
 				}
 			} catch (SQLException e) {
-				System.out.println("Failed to show number of average draws");
+				System.out.println("Failed to show number of average draws.");
 			}finally {
 				if (stmt != null) { stmt.close();
 				}
@@ -150,8 +156,25 @@ public class TopTrumpsDatabase {
 			return numberOfDraws;
 		}
 		
-		//Nana, query to return the largest number of rounds played in a single game
+		//query to return the largest number of rounds played in a single game
 		public int getNumberOfRoundsPlayedInGame() throws SQLException {
+			Statement stmt = null;
+			String queryFive = "select MAX(NUMBER_OF_ROUNDS)" +
+							   "from GameStats";
+			try {
+				stmt = connect().createStatement();
+				ResultSet rs = stmt.executeQuery(queryFive);
+				while(rs.next()) {
+					int numberOfRoundsPlayedInGame = rs.getInt("NUMBER_OF_ROUNDS");
+					System.out.println(numberOfRoundsPlayedInGame + "\t");
+				}
+			 	
+			} catch (SQLException e) {
+				System.out.println("Failed to show number of average draws.");
+			}finally {
+				if (stmt !=null ) { stmt.close();
+				}
+		    }
 			return numberOfRoundsPlayedInGame;	
-		}	
+		 }	
 }
