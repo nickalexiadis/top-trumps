6398920b4017780a5edf1196cac696878645250c
Miruna Lazăr <56715525+mirunalazar3@users.noreply.github.com>
2020-01-31 17:30:18 +0000

    Update TopTrumpsDatabase.java
    I didn't try it on the lab machine yet, but it has the yacata "connection url" probably will have to edit out the main method and just add one of our username and passwords in the variable area.

--- TopTrumpsDatabase.java
+++ TopTrumpsDatabase.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 //This class will establish the connection between the Database and the game, it will update the stats in the database.
  
@@ -14,25 +15,38 @@ public class TopTrumpsDatabase {
 		private int numberOfGames;
 		private int computerWins;
 		private int humanWins;
-		private double numberOfDraws;
+		private int numberOfDraws;
 		private int numberOfRoundsPlayedInGame;
 		
 		//defining the attributes for the connection: URL, user and password
 		//create a variable for the connection along with the user ID and password 
-		    private String connectionURL = "Add connection URL";
+		//Question: do we need connection URL?
 		    private String username = "Add username";
 		    private String password ="Add password";
+		  
+		    //testing the database
+		    public static void main(String args[]) {
+		    	// input from standard in
+		    	Scanner input = new Scanner(System.in);
+		    	//user enters user name
+		    	System.out.print("Enter Username: ");
+		    	String username = input.nextLine();
+		    	//user enters password
+		    	System.out.print("Enter Password: ");
+		    	String password = input.nextLine();
+		    	System.out.println();
+		    	input.close();
+		    }
 		    
 		//setup the database connection, reference: https://www.postgresqltutorial.com/postgresql-jdbc/connecting-to-postgresql-database/
 		 public Connection connect() {
 		        Connection connection = null;
 		        try {
-		            connection = DriverManager.getConnection(connectionURL, username, password);
+		            connection = DriverManager.getConnection("jdbc:post" + "gresql://yacata.dcs.gla.ac.uk:5432/", username, password);
 		            System.out.println("Connected to the PostgreSQL server successfully.");
 		        } catch (SQLException e) {
 		            System.out.println(e.getMessage());
 		        }
-		 
 		        return connection;
 		    }
 
@@ -50,17 +64,18 @@ public class TopTrumpsDatabase {
 		}
 		
 		//update the database, Question: How do we actually connect the game to the database? So as to update stats with each game played?
-		 public int updateGameStats(int numberOfGames, int computerWins, int humanWins, double numberOfDraws, int numberOfRoundsPlayedInGame) {
+		 public int updateGameStats(int numberOfGames, int computerWins, int humanWins, int numberOfDraws, int numberOfRoundsPlayedInGame) {
 		        String SQL = "UPDATE NUMBER_OF_GAMES, COMPUTER_WINS, HUMAN_WINS, DRAWS, NUMBER_OF_ROUNDS";
 		        int affectedrows = 0;
 		        try (Connection connection = connect();
 		                java.sql.PreparedStatement pstmt = connection.prepareStatement(SQL)) {
 		 
-		            pstmt.setInt(0, numberOfGames);
-		            pstmt.setInt(0, computerWins);
-		            pstmt.setInt(0, humanWins);
-		            pstmt.setDouble(0, numberOfDraws);
-		            pstmt.setInt(0, numberOfRoundsPlayedInGame);
+		            //need to create counters for the number of games, computer wins, human wins, average number of draws, number of rounds in a game
+					pstmt.setInt(gamesCounter(), numberOfGames); 
+		            pstmt.setInt(computerWinCounter(), computerWins);
+		            pstmt.setInt(humanWinCounter(), humanWins);
+		            pstmt.setInt(drawsCounter(), numberOfDraws);
+		            pstmt.setInt(roundsCounter(), numberOfRoundsPlayedInGame);
 		 
 		            affectedrows = pstmt.executeUpdate();
 		 
@@ -69,13 +84,44 @@ public class TopTrumpsDatabase {
 		        }
 		        return affectedrows;
 		    }
+		//we need counters for the update above
+		private int gamesCounter() {
+			int game = 0;
+			game ++;
+			return game;
+		}
 		
+		private int computerWinCounter() {
+			int computerWin = 0;
+			computerWin ++;
+			return computerWin;
+		}
+		
+		private int humanWinCounter() {
+			int humanWin = 0;
+			humanWin ++;
+			return humanWin;
+		}
+		
+		private int drawsCounter() {
+			int draws = 0;
+			draws ++;
+			return draws;
+			
+		}
+		
+		private int roundsCounter() {
+			int rounds = 0;
+			rounds ++;
+			return rounds;
+		}
+		 
 		//reference for the following getters: https://docs.oracle.com/javase/tutorial/jdbc/basics/processingsqlstatements.html
 		//query to return the number of games played overall  
 		public int getNumberOfGames() throws SQLException {
 			Statement stmt = null;
-			String queryOne = "select SUM(NUMBER_OF_GAMES)" +
-							  "from GameStats"; 
+			String queryOne = "select COUNT(GAME_ID)" +
+							  "from GAME_STATS"; 
 			try {
 				stmt = connect().createStatement();
 				ResultSet rs = stmt.executeQuery(queryOne);
@@ -95,8 +141,9 @@ public class TopTrumpsDatabase {
 		//query to return how many times the computer has won 
 		public int getComputerWins() throws SQLException {
 			Statement stmt = null;
-			String queryTwo = "select SUM(COMPUTER_WINS)" + 
-							  "from GameStats";
+			String queryTwo = "select COUNT(GAME_ID)" + 
+							  "from GAME_STATS" +
+							  "where winner = 'COMPUTER'";
 			try {
 				stmt = connect().createStatement();
 				ResultSet rs = stmt.executeQuery(queryTwo);
@@ -116,8 +163,10 @@ public class TopTrumpsDatabase {
 		//query to return how many times the human has won  
 		public int getHumanWins() throws SQLException {
 			Statement stmt = null;
-			String queryThree = "select SUM(HUMAN_WINS)" +
-								"from GameStats";
+			String queryThree = "select COUNT(WINS)" +
+								"from GAME_STATS" +
+								"where winner = 'HUMAN";
+								
 			try {
 				stmt = connect().createStatement();
 				ResultSet rs = stmt.executeQuery(queryThree);
@@ -134,11 +183,11 @@ public class TopTrumpsDatabase {
 			return humanWins;
 		}
 		
-		//query to return the average number of draws, done by James
+		//James, query to return the average number of draws 
 		public double getNumberOfDraws() throws SQLException {
 			Statement stmt = null;
 			String queryFour = "select AVG(DRAWS)" + 
-							   "from GameStats";
+							   "from GAME_STATS";
 
 			try {
 				stmt = connect().createStatement();
@@ -160,7 +209,7 @@ public class TopTrumpsDatabase {
 		public int getNumberOfRoundsPlayedInGame() throws SQLException {
 			Statement stmt = null;
 			String queryFive = "select MAX(NUMBER_OF_ROUNDS)" +
-							   "from GameStats";
+							   "from GAME_STATS";
 			try {
 				stmt = connect().createStatement();
 				ResultSet rs = stmt.executeQuery(queryFive);
