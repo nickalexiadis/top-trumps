25ac5c42f5e545cfd252f026d3a1f77cf3e9a765
Miruna Lazăr <56715525+mirunalazar3@users.noreply.github.com>
2020-02-08 12:55:49 +0000

    Update TopTrumpsDatabase.java

--- TopTrumpsDatabase.java
+++ TopTrumpsDatabase.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
+import java.util.Random;
 import com.mysql.jdbc.PreparedStatement;
 
 //This class will establish the connection between the Database and the game, it will update the stats in the database.
@@ -13,22 +14,26 @@ import com.mysql.jdbc.PreparedStatement;
 public class TopTrumpsDatabase {
 
 		//database columns, declared as variables 
-		private int draws;
-		private int game_winner;
-		private int user_round_wins;
-		private int ai1_round_wins;
-		private int ai2_round_wins;
-		private int ai3_round_wins;
-		private int ai4_round_wins;
-		private int round_total;
-		
+		private double draws;
+		//private int gameWinner;
+		private int userRoundWins;
+		private int ai1RoundWins;
+		private int ai2RoundWins;
+		private int ai3RoundWins;
+		private int ai4RoundWins;
+		private int roundTotal;
+		private int numberOfGames;
 	
 		//defining the attributes for the connection: user and password
 
 		    private String username = "m_19_2444965h";
 		    private String password ="2444965h";
 		    private String dbName = "m_19_2444965h";
-
+		   
+		    
+		    public TopTrumpsDatabase() {
+		    	
+		    }
 
 		    //testing the database
 		    public static void main(String args[]) {
@@ -54,52 +59,58 @@ public class TopTrumpsDatabase {
 			
 		
 		//insert the values in the database, a bit updated, reference is: https://www.postgresqltutorial.com/postgresql-jdbc/insert/
-		public int insertDatabase(int draws, int game_winner, int user_round_wins, int ai1_round_wins, int ai2_round_wins, int ai3_round_wins, int ai4_round_wins) {
-			String SQL = "BEGIN;" + "INSERT INTO GAME_STATS(DRAWS,GAME_WINNER,ROUND_TOTAL,USER_ROUND_WINS,AI1_ROUND_WINS,AI2_ROUND_WINS,AI3_ROUND_WINS,AI4_ROUND_WINS)" + "VALUESVALUES (?,?,?,?,?,?,?)" + "COMMIT;";
-
-			int id = 0;
+		public void insertDatabase(int draws, String gameWinner, int roundTotal, int userRoundWins, int ai1RoundWins, int ai2RoundWins, int ai3RoundWins, int ai4RoundWins) {
+//			String SQL = "BEGIN;" + "INSERT INTO GAME_STATS(GAME_ID, DRAWS,GAME_WINNER,ROUND_TOTAL,USER_ROUND_WINS,AI1_ROUND_WINS,AI2_ROUND_WINS,AI3_ROUND_WINS,AI4_ROUND_WINS)" + "VALUES (?,?,?,?,?,?,?,?,?)" + "COMMIT;";
+			String SQL = "INSERT INTO GAME_STATS(GAME_ID, DRAWS,GAME_WINNER,ROUND_TOTAL,USER_ROUND_WINS,AI1_ROUND_WINS,AI2_ROUND_WINS,AI3_ROUND_WINS,AI4_ROUND_WINS)" + "VALUES (?,?,?,?,?,?,?,?,?)";
 
 	        try (Connection connect = connect();
 	            java.sql.PreparedStatement pstmt = connect.prepareStatement(SQL,Statement.RETURN_GENERATED_KEYS)) {
-
+	        	pstmt.setInt(1, numberOfGames);
 	            pstmt.setInt(2, draws);
-	            pstmt.setInt(3, game_winner);
-	            pstmt.setInt(4, user_round_wins);
-	            pstmt.setInt(5, ai1_round_wins);
-	            pstmt.setInt(6, ai2_round_wins);
-	            pstmt.setInt(7, ai3_round_wins);
-	            pstmt.setInt(8, ai4_round_wins);
+	            pstmt.setString(3, gameWinner);
+	            pstmt.setInt(4, roundTotal);
+	            pstmt.setInt(5, userRoundWins);
+	            pstmt.setInt(6, ai1RoundWins);
+	            pstmt.setInt(7, ai2RoundWins);
+	            pstmt.setInt(8, ai3RoundWins);
+	            pstmt.setInt(9, ai4RoundWins);
 
 	            //executes updates
 	            int affectedRows = pstmt.executeUpdate();
-	          
-	            //will check the affected rows
-	            if(affectedRows > 0) {
-	                //will get the id back 
-	            	try(ResultSet rs = pstmt.getGeneratedKeys()) {
-	                    if(rs.next()) {
-	                        id = rs.getInt(1);
-	                    }
-	                } catch (SQLException ex) {
-	                    System.out.println(ex.getMessage());
-	                }
-	            }
+	        
 	        } catch (SQLException ex) {
 	            System.out.println(ex.getMessage());
+	            System.out.println(ex);
 	        }
-	        return id;
 
 	    }
-
 		//reference for the following getters: https://docs.oracle.com/javase/tutorial/jdbc/basics/processingsqlstatements.html
+		//query to return the number of games played overall  
+			public int getNumberOfGames() throws SQLException {
+				Statement stmt = null;
+				String queryOne = "select COUNT(GAME_ID)" + "from public.game_stats"; 
+				try {
+					stmt = connect().createStatement();
+					ResultSet rs = stmt.executeQuery(queryOne);
+					while(rs.next()) {
+						int numberOfGames = rs.getInt("NUMBER_OF_GAMES");
+						System.out.println(numberOfGames+ "\t");
+					}
+				} catch (SQLException e) {
+					System.out.println("Failed to show number of games");	
+				}finally {
+					if (stmt != null) { stmt.close();
+				        }
+				  }
+				return numberOfGames;
+			 }
 		//query to return how many times the AI1 has won 
 		public int getAI1Wins() throws SQLException {
 			
 			Statement stmt = null;
 			
-			String queryOne = "select COUNT(GAME_ID)" + 
-							  "from GAME_STATS" +
-							  "where GAME_WINNER = 'AI1'";
+			String queryOne = "select COUNT(GAME_ID) from public.game_stats where GAME_WINNER = 'AI Player 1'";
+
 			try {
 				
 				stmt = connect().createStatement();
@@ -107,11 +118,11 @@ public class TopTrumpsDatabase {
 				
 				while(rs.next()) {
 					
-					int ai1_round_wins = rs.getInt("AI1_ROUND_WINS");
-					System.out.println(ai1_round_wins + "\t");
+					int ai1_round_wins = rs.getInt("count");
+					System.out.println("AI1 Wins:" + ai1_round_wins + "\t");
 				}
 			} catch (SQLException e) {
-				
+				System.out.println(e);
 				System.out.println("Failed to show number of computer wins.");
 		
 			}finally {
@@ -119,16 +130,14 @@ public class TopTrumpsDatabase {
 				if (stmt != null) { stmt.close();
 			        }
 			  }
-			return ai1_round_wins;
+			return ai1RoundWins;
 		}
 		//query to return how many times the AI2 has won 
 				public int getAI2Wins() throws SQLException {
 					
 					Statement stmt = null;
 					
-					String queryTwo = "select COUNT(GAME_ID)" + 
-									  "from GAME_STATS" +
-									  "where GAME_WINNER = 'AI2'";
+					String queryTwo = "select COUNT(GAME_ID) from public.game_stats where GAME_WINNER = 'AI Player 2'";
 					try {
 						
 						stmt = connect().createStatement();
@@ -136,11 +145,11 @@ public class TopTrumpsDatabase {
 						
 						while(rs.next()) {
 							
-							int ai2_round_wins = rs.getInt("AI2_ROUND_WINS");
-							System.out.println(ai2_round_wins + "\t");
+							int ai2_round_wins = rs.getInt("count");
+							System.out.println("AI2 Wins:" + ai2_round_wins + "\t");
 						}
 					} catch (SQLException e) {
-						
+						System.out.println(e);
 						System.out.println("Failed to show number of computer wins.");
 					
 					}finally {
@@ -148,15 +157,13 @@ public class TopTrumpsDatabase {
 						if (stmt != null) { stmt.close();
 					        }
 					  }
-					return ai2_round_wins;
+					return ai2RoundWins;
 				}
 			//query to return how many times the AI3 has won 
 				public int getAI3Wins() throws SQLException {
 					
 					Statement stmt = null;
-					String queryThree = "select COUNT(GAME_ID)" + 
-									  "from GAME_STATS" +
-									  "where GAME_WINNER = 'AI1'";
+					String queryThree = "select COUNT(GAME_ID) from public.game_stats where GAME_WINNER = 'AI Player 3'";
 					
 					try {
 						
@@ -164,12 +171,12 @@ public class TopTrumpsDatabase {
 						ResultSet rs = stmt.executeQuery(queryThree);
 						
 						while(rs.next()) {
-							int ai3_round_wins = rs.getInt("AI3_ROUND_WINS");
-							System.out.println(ai3_round_wins + "\t");
+							int ai3_round_wins = rs.getInt("count");
+							System.out.println("AI3 Wins:" + ai3_round_wins + "\t");
 						
 						}
 					} catch (SQLException e) {
-						
+						System.out.println(e);
 						System.out.println("Failed to show number of computer wins.");
 					
 					}finally {
@@ -177,15 +184,14 @@ public class TopTrumpsDatabase {
 						if (stmt != null) { stmt.close();
 					        }
 					  }
-					return ai3_round_wins;
+					return ai3RoundWins;
 				}
 			//query to return how many times the AI4 has won 
 				public int getAI4Wins() throws SQLException {
 					
 					Statement stmt = null;
-					String queryFour = "select COUNT(GAME_ID)" + 
-									  "from GAME_STATS" +
-									  "where GAME_WINNER = 'AI4'";
+					String queryFour = "select COUNT(GAME_ID) from public.game_stats where GAME_WINNER = 'AI Player 4'";
+
 					try {
 						
 						stmt = connect().createStatement();
@@ -193,11 +199,11 @@ public class TopTrumpsDatabase {
 						
 						while(rs.next()) {
 							
-							int ai4_round_wins = rs.getInt("AI4_ROUND_WINS");
-							System.out.println(ai4_round_wins + "\t");
+							int ai4_round_wins = rs.getInt("count");
+							System.out.println("AI4 Wins:" + ai4_round_wins + "\t");
 						}
 					} catch (SQLException e) {
-					
+						System.out.println(e);
 						System.out.println("Failed to show number of computer wins.");
 					
 					}finally {
@@ -205,27 +211,25 @@ public class TopTrumpsDatabase {
 						if (stmt != null) { stmt.close();
 					        }
 					  }
-					return ai4_round_wins;
+					return ai4RoundWins;
 				}
 
 		//query to return how many times the human has won  
 		public int getHumanWins() throws SQLException {
 			
 			Statement stmt = null;
-			String queryFive = "select COUNT(WINS)" +
-								"from GAME_STATS" +
-								"where GAME_WINNER = 'User";
+			String queryFive = "select COUNT(user_round_wins) from public.game_stats where GAME_WINNER = 'User'";
 
 			try {
 				stmt = connect().createStatement();
 				ResultSet rs = stmt.executeQuery(queryFive);
 				
 				while(rs.next()) {
-					int user_round_wins = rs.getInt("USER_ROUND_WINS");
-					System.out.println(user_round_wins + "\t");
+					int user_round_wins = rs.getInt("count");
+					System.out.println("User Wins:" + user_round_wins + "\t");
 				}
 			} catch (SQLException e) {
-				
+				System.out.println(e);
 				System.out.println("Failed to show number of human wins.");
 			
 			}finally {
@@ -233,15 +237,14 @@ public class TopTrumpsDatabase {
 				if (stmt != null) { stmt.close();
 			        }
 			  }
-			return user_round_wins;
+			return userRoundWins;
 		}
 
 		//query to return the average number of draws 
 		public double getNumberOfDraws() throws SQLException {
 			
 			Statement stmt = null;
-			String querySix = "select AVG(DRAWS)" + 
-							   "from GAME_STATS";
+			String querySix = "select AVG(DRAWS) from public.GAME_STATS";
 
 			try {
 				stmt = connect().createStatement();
@@ -249,11 +252,11 @@ public class TopTrumpsDatabase {
 				
 				while(rs.next()) {
 					
-					double draws = rs.getDouble("DRAWS");
-					System.out.println(draws + "\t");
+					double draws = rs.getDouble("avg");
+					System.out.println("Average draws:"+draws + "\t");
 				}
 			} catch (SQLException e) {
-				
+				System.out.println(e);
 				System.out.println("Failed to show number of average draws.");
 			
 			}finally {
@@ -268,8 +271,7 @@ public class TopTrumpsDatabase {
 		public int getNumberOfRoundsPlayedInGame() throws SQLException {
 			
 			Statement stmt = null;
-			String querySeven = "select MAX(ROUND_TOTAL)" +
-							   "from GAME_STATS";
+			String querySeven = "select MAX(ROUND_TOTAL) from public.GAME_STATS";
 			try {
 				
 				stmt = connect().createStatement();
@@ -277,20 +279,20 @@ public class TopTrumpsDatabase {
 				
 				while(rs.next()) {
 					
-					int round_total = rs.getInt("ROUNDS_TOTAL");
-					System.out.println(round_total + "\t");
+					int round_total = rs.getInt("max");
+					System.out.println("Max number of Total rounds:"+ round_total + "\t");
 				}
 
 			} catch (SQLException e) {
 				
-				System.out.println("Failed to show number of average draws.");
+				System.out.println("Failed to show max number of total rounds.");
 			
 			}finally {
 				
 				if (stmt !=null ) { stmt.close();
 				}
 		    }
-			return round_total;	
+			return roundTotal;	
 		 }		
 		//closing the database connection
 				public void closeConenction() {
